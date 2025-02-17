da42ccabf6fba11c786cf5fd7a12cf57fffcca6d
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2025-02-17 11:44:25 +0300

    CXBOX-797 | Synchronous and asynchronous requests

--- src/epics/view/waitUntil.ts
+++ src/epics/view/waitUntil.ts
@@ -1,9 +1,25 @@
-import { BcDataResponse, CXBoxEpic, OperationPostInvokeWaitUntil, Store } from '../../interfaces'
-import { catchError, concat, delay, EMPTY, filter, map, mergeMap, Observable, of, timeout, TimeoutError } from 'rxjs'
+import { BcDataResponse, BcMetaState, CXBoxEpic, OperationPostInvokeWaitUntil, Store } from '../../interfaces'
+import {
+    catchError,
+    concat,
+    delay,
+    EMPTY,
+    filter,
+    map,
+    mergeMap,
+    Observable,
+    of,
+    switchMap,
+    take,
+    takeUntil,
+    timeout,
+    TimeoutError
+} from 'rxjs'
 import { bcForceUpdate, closeViewPopup, showViewPopup, waitUntil, WaitUntilPopupOptions } from '../../actions'
 import { buildBcUrl, createApiErrorObservable, getFilters, getSorters } from '../../utils'
 import { DataItem } from '@cxbox-ui/schema'
 import { Api } from '../../api'
+import { StateObservable } from 'redux-observable'
 
 const fetchDataRequest = <S extends Store, A extends Api>(bcName: string, state: S, api: A) => {
     const bc = state.screen.bo.bc[bcName]
@@ -27,13 +43,13 @@ const recursiveQueryWithRepeat = <S extends Store, A extends Api>(
     state: S,
     api: A,
     postInvoke: Omit<OperationPostInvokeWaitUntil, 'type'>,
-    bcName: string
+    currentBcName: string
 ): Observable<BcDataResponse> => {
     const { timeout: repeatTimeout = 5000, timeoutMaxRequests = 5 } = postInvoke
-    const { successCondition_bcName, successCondition_fieldKey, successCondition_value } = postInvoke
+    const { successCondition_fieldKey, successCondition_value } = postInvoke
     const requestInterval = Math.floor(+repeatTimeout / +timeoutMaxRequests)
 
-    return fetchDataRequest(successCondition_bcName ?? bcName, state, api).pipe(
+    return fetchDataRequest(currentBcName, state, api).pipe(
         mergeMap(response => {
             const data = response.data
 
@@ -41,63 +57,106 @@ const recursiveQueryWithRepeat = <S extends Store, A extends Api>(
                 ? of(response)
                 : of(null).pipe(
                       delay(requestInterval),
-                      mergeMap(() => recursiveQueryWithRepeat(state, api, postInvoke, bcName))
+                      mergeMap(() => recursiveQueryWithRepeat(state, api, postInvoke, currentBcName))
                   )
         }),
         timeout(+repeatTimeout)
     )
 }
 
+const getBcHierarchyArr = (bcDictionary: Record<string, BcMetaState>, bcName: string): string[] => {
+    const bcHierarchy: string[] = [bcName]
+    let parentBcName = bcDictionary[bcName]?.parentName
+
+    while (parentBcName) {
+        bcHierarchy.push(parentBcName)
+
+        parentBcName = bcDictionary[parentBcName]?.parentName
+    }
+
+    return bcHierarchy.reverse()
+}
+
+const checkingReadinessForBuildBcUrl =
+    (bcName: string) =>
+    <S extends Store>(state: S) => {
+        const bcDictionary = state.screen.bo.bc
+        const bcNamesForChecking = getBcHierarchyArr(bcDictionary, bcName)
+
+        return bcNamesForChecking.every(
+            bcNameForChecking => bcDictionary[bcNameForChecking]?.loading === false && bcDictionary[bcNameForChecking]?.cursor
+        )
+    }
+
+const getCheckedStateForBuildBcUrl = (state$: StateObservable<Store>, currentBcName: string) => {
+    const currentState = state$.value
+
+    return checkingReadinessForBuildBcUrl(currentBcName)(currentState)
+        ? of(currentState)
+        : state$.pipe(filter(checkingReadinessForBuildBcUrl(currentBcName)), take(1))
+}
+
 export const waitUntilEpic: CXBoxEpic = (action$, state$, { api }) =>
     action$.pipe(
         filter(waitUntil.match),
         mergeMap(action => {
-            const { bcName, postInvoke } = action.payload
+            const { bcName: actionBcName, postInvoke } = action.payload
+            const { successCondition_bcName } = postInvoke
+            const currentBcName = successCondition_bcName ?? actionBcName
 
-            const showPopup = (status: WaitUntilPopupOptions['status'], message?: string) => {
-                const popupIsActive = state$.value.view.popupData.type === 'waitUntil'
+            const switchPopup = (status: WaitUntilPopupOptions['status'], message?: string) => {
                 const isProgressStatus = status === 'progress'
                 const isFinalStatus = !isProgressStatus
 
-                if (isProgressStatus || (isFinalStatus && message && popupIsActive)) {
+                if (isProgressStatus || (isFinalStatus && message)) {
                     return of(
                         showViewPopup({
                             type: 'waitUntil',
-                            bcName,
-                            calleeBCName: bcName,
+                            bcName: actionBcName,
+                            calleeBCName: actionBcName,
                             options: { status, message }
                         })
                     )
                 }
 
-                if (isFinalStatus && !message && popupIsActive) {
-                    return of(closeViewPopup({ bcName }))
+                if (isFinalStatus && !message) {
+                    return of(closeViewPopup({ bcName: actionBcName }))
                 }
 
                 return EMPTY
             }
 
-            const forceUpdate = () => {
-                const popupIsActive = state$.value.view.popupData.type === 'waitUntil'
+            const forceUpdate = (bcName: string) => {
                 const currentBcIsExist = state$.value.screen.bo.bc[bcName]
 
-                return currentBcIsExist && popupIsActive ? of(bcForceUpdate({ bcName })) : EMPTY
+                return currentBcIsExist ? of(bcForceUpdate({ bcName })) : EMPTY
             }
 
-            const requestResult = recursiveQueryWithRepeat(state$.value, api, postInvoke, bcName).pipe(
-                map(response => response.data),
-                mergeMap(data => {
-                    return concat(forceUpdate(), showPopup('success', postInvoke.successMessage))
-                }),
-                catchError(error => {
-                    if (error instanceof TimeoutError) {
-                        return concat(forceUpdate(), showPopup('timeout', postInvoke.timeoutMessage))
-                    }
+            const requestResult = getCheckedStateForBuildBcUrl(state$, currentBcName).pipe(
+                switchMap(checkedState => {
+                    return recursiveQueryWithRepeat(checkedState, api, postInvoke, currentBcName).pipe(
+                        takeUntil(
+                            state$.pipe(
+                                filter(() => {
+                                    return state$.value.view.popupData.type !== 'waitUntil'
+                                })
+                            )
+                        ),
+                        map(response => response.data),
+                        mergeMap(data => {
+                            return concat(forceUpdate(currentBcName), switchPopup('success', postInvoke.successMessage))
+                        }),
+                        catchError(error => {
+                            if (error instanceof TimeoutError) {
+                                return concat(forceUpdate(currentBcName), switchPopup('timeout', postInvoke.timeoutMessage))
+                            }
 
-                    return concat(of(closeViewPopup({ bcName })), createApiErrorObservable(error))
+                            return concat(of(closeViewPopup({ bcName: actionBcName })), createApiErrorObservable(error))
+                        })
+                    )
                 })
             )
 
-            return concat(showPopup('progress', postInvoke.inProgressMessage), requestResult)
+            return concat(switchPopup('progress', postInvoke.inProgressMessage), requestResult)
         })
     )
