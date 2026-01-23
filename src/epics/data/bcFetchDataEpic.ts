db29e0ac987b31411ca3458b2e4e39151338ba5a
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2026-01-23 18:46:18 +0300

    CXBOX-1166 | fix

--- src/epics/data/bcFetchDataEpic.ts
+++ src/epics/data/bcFetchDataEpic.ts
@@ -13,13 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { catchError, concat, EMPTY, filter, mergeMap, of, race } from 'rxjs'
 import { AnyAction } from 'redux'
 import {
     buildBcUrl,
     checkShowCondition,
-    CursorSelectionStrategy,
     cursorStrategyManager,
     getEagerBcChildren,
     getFilters,
